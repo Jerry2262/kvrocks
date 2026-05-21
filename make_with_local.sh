4896c5b1a8883b151fb3604d010d856c17b4f0ad
shixianjie <shixianjie1@huawei.com>
2026-05-21 15:24:32 +0800

    Add gcc-toolset-14 environment sourcing in make_with_local.sh

--- make_with_local.sh
+++ make_with_local.sh
@@ -1,4 +1,5 @@
 rm -rf build/
+source /opt/openEuler/gcc-toolset-14/enable
 ./x.py build -j`nproc` \
 -D FETCHCONTENT_SOURCE_DIR_ROCKSDB=../../rocksdb \
 -D CMAKE_CXX_FLAGS="-march=armv8.3-a+rcpc -w" \
