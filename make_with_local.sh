bf42ce4dc9a7395dc41c09894b3aaf645bde3289
shixianjie <shixianjie1@huawei.com>
2026-05-21 17:22:33 +0800

    Make GCC toolset-14 and march flags conditional on aarch64, add --unittest to build

--- make_with_local.sh
+++ make_with_local.sh
@@ -1,6 +1,12 @@
 rm -rf build/
-[ -f /opt/openEuler/gcc-toolset-14/enable ] && source /opt/openEuler/gcc-toolset-14/enable
+
+march_flags=""
+if [ -f /opt/openEuler/gcc-toolset-14/enable ] && [ "$(uname -m)" = "aarch64" ]; then
+    source /opt/openEuler/gcc-toolset-14/enable
+    march_flags="-march=armv8.3-a+rcpc"
+fi
+
 ./x.py build -j`nproc` --unittest \
 -D FETCHCONTENT_SOURCE_DIR_ROCKSDB=../../rocksdb \
--D CMAKE_CXX_FLAGS="-march=armv8.3-a+rcpc -w" \
--D CMAKE_C_FLAGS="-march=armv8.3-a+rcpc -w"
+-D CMAKE_CXX_FLAGS="$march_flags -w" \
+-D CMAKE_C_FLAGS="$march_flags -w"
