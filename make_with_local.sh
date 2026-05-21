a12df4dad6172deca65eb8dd71f9537cf82ab223
shixianjie <shixianjie1@huawei.com>
2026-05-21 16:36:35 +0800

    make source of gcc-toolset-14 optional when file is missing

--- make_with_local.sh
+++ make_with_local.sh
@@ -1,5 +1,5 @@
 rm -rf build/
-source /opt/openEuler/gcc-toolset-14/enable
+[ -f /opt/openEuler/gcc-toolset-14/enable ] && source /opt/openEuler/gcc-toolset-14/enable
 ./x.py build -j`nproc` \
 -D FETCHCONTENT_SOURCE_DIR_ROCKSDB=../../rocksdb \
 -D CMAKE_CXX_FLAGS="-march=armv8.3-a+rcpc -w" \
